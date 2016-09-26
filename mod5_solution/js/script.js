f49b601e7cf12901e7962a0f360969805e086f95
rbrownhill <robertbrownhill@yahoo.com>
2016-09-26 16:03:09 -0700

    Update script.js

--- mod5_solution/js/script.js
+++ mod5_solution/js/script.js
@@ -103,7 +103,7 @@ function buildAndShowHomeHTML (categories) {
       // variable's name implies it expects.
       // var chosenCategoryShortName = ....
       var chosenCategoryShortName =
-           "&quot" +  chooseRandomCategory(categories).short_name + "&quot";
+           "'" +  chooseRandomCategory(categories).short_name + "'";
       
      console.log(chosenCategoryShortName);
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
