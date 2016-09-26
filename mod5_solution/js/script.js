51ecdfa7721173f503c55af7a3475b2bd0fd6fa1
rbrownhill <robertbrownhill@yahoo.com>
2016-09-26 15:41:04 -0700

    Update script.js

--- mod5_solution/js/script.js
+++ mod5_solution/js/script.js
@@ -105,8 +105,8 @@ function buildAndShowHomeHTML (categories) {
       var chosenCategoryShortName =
             chooseRandomCategory(categories).short_name;
 
-  //    console.log("Hello1");
-  //    console.log(chosenCategoryShortName);
+      console.log("Hello1");
+     console.log(chosenCategoryShortName);
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
       // chosen category from STEP 2. Use existing insertProperty function for that purpose.
       // Look through this code for an example of how to do use the insertProperty function.
@@ -124,7 +124,7 @@ function buildAndShowHomeHTML (categories) {
                  "randomCategoryShortName",
                 chosenCategoryShortName);
       
-   //  console.log(homeHtmlToInsertIntoMainPage);
+     console.log(homeHtmlToInsertIntoMainPage);
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
